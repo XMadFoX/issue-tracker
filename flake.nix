499d5e57a093aae83e94ab97a12ec6b2ef8bc021
MadFox <madfox@laserflare.net>
2025-10-04 23:56:24 +0400

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Development environment with Node.js, pnpm, bun, and more";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            nodejs_24
+            pnpm_10
+            bun
+            git
+            toybox
+            lefthook
+            biome
+            commitlint-rs
+          ];
+        };
+      }
+    );
+}
+
