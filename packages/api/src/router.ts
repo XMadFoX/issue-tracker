5267460ec1c6b4e4c38f88c8c1a2305f24737b6f
MadFox <madfox@laserflare.net>
2025-10-16 03:28:16 +0400

    feat(api/team): add to the main router

--- packages/api/src/router.ts
+++ packages/api/src/router.ts
@@ -1,3 +1,4 @@
+import { teamRouter } from "./features/teams/router";
 import { workspaceRouter } from "./features/workspaces/router";
 
-export const router = { workspace: workspaceRouter };
+export const router = { workspace: workspaceRouter, team: teamRouter };
