fb9da6855b7b390106a83f22e4e1f7ecd15a9873
MadFox <madfox@laserflare.net>
2025-11-05 23:49:02 +0400

    feat(api/team-memb): init router

--- packages/api/src/router.ts
+++ packages/api/src/router.ts
@@ -1,4 +1,5 @@
 import { rolePermissionsRouter } from "./features/permissions/router";
+import { teamMembershipRouter } from "./features/team-memberships/router";
 import { teamRouter } from "./features/teams/router";
 import { workspaceMembershipRouter } from "./features/workspace-memberships/router";
 import { workspaceRouter } from "./features/workspaces/router";
@@ -9,4 +10,5 @@ export const router = {
 		permissions: rolePermissionsRouter,
 	},
 	workspaceMembership: workspaceMembershipRouter,
+	teamMembership: teamMembershipRouter,
 };
