afa152bcecfa031702116c8cb9c91e121a8fcc82
MadFox <madfox@laserflare.net>
2025-11-14 00:21:43 +0400

    fix(api): add missing router imports

--- packages/api/src/router.ts
+++ packages/api/src/router.ts
@@ -1,10 +1,13 @@
 import { rolePermissionsRouter } from "./features/permissions/router";
+import { roleRouter } from "./features/roles/router";
 import { teamMembershipRouter } from "./features/team-memberships/router";
 import { teamRouter } from "./features/teams/router";
 import { workspaceMembershipRouter } from "./features/workspace-memberships/router";
 import { workspaceRouter } from "./features/workspaces/router";
 
 export const router = {
+	workspace: workspaceRouter,
+	team: teamRouter,
 	role: {
 		...roleRouter,
 		permissions: rolePermissionsRouter,
