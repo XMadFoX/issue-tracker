fe53bc9b061b6d21fb93ab4fa43a1c82f1a96e05
MadFox <madfox@laserflare.net>
2025-11-17 23:58:15 +0400

    refactor(api/workspace-memb): infer insert schema

--- packages/api/src/features/workspace-memberships/schema.ts
+++ packages/api/src/features/workspace-memberships/schema.ts
@@ -1,6 +1,5 @@
 import { workspaceMembership } from "db/features/tracker/tracker.schema";
 import { createInsertSchema } from "drizzle-zod";
-import { z } from "zod/v4";
 import { roleInsertSchema } from "../roles/schema";
 
 export const workspaceMembershipInsertSchema =
@@ -12,20 +11,23 @@ export const workspaceMembershipCreateSchema = workspaceMembershipInsertSchema
 		roleId: roleInsertSchema.shape.id.optional(), // Optional, can be auto-assigned
 	});
 
-export const workspaceMembershipListSchema = z.object({
-	workspaceId: z.string(),
-});
+export const workspaceMembershipListSchema =
+	workspaceMembershipInsertSchema.pick({
+		workspaceId: true,
+	});
 
-export const workspaceMembershipGetSchema = z.object({
-	id: z.string(),
-	workspaceId: z.string(),
-});
+export const workspaceMembershipGetSchema =
+	workspaceMembershipInsertSchema.pick({
+		id: true,
+		workspaceId: true,
+	});
 
 export const workspaceMembershipUpdateSchema = workspaceMembershipInsertSchema
 	.partial()
 	.required({ id: true, workspaceId: true });
 
-export const workspaceMembershipDeleteSchema = z.object({
-	id: z.string(),
-	workspaceId: z.string(),
-});
+export const workspaceMembershipDeleteSchema =
+	workspaceMembershipInsertSchema.pick({
+		id: true,
+		workspaceId: true,
+	});
