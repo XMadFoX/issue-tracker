75cd12c654b32cb5cafa85fa9d897376a62c49f8
MadFox <madfox@laserflare.net>
2025-11-23 01:41:09 +0400

    feat(ui/form): add basic form & fields components

---
+++ packages/ui/src/components/form/form-base.tsx
@@ -0,0 +1,62 @@
+import type { HTMLInputTypeAttribute, ReactNode } from "react"
+import { useFieldContext } from "./form-hooks"
+import {
+  Field,
+  FieldContent,
+  FieldDescription,
+  FieldError,
+  FieldLabel,
+} from "../field"
+
+export type FormControlProps = {
+  label: string
+  description?: string
+  type?: HTMLInputTypeAttribute
+}
+
+type FormBaseProps = FormControlProps & {
+  children: ReactNode
+  horizontal?: boolean
+  controlFirst?: boolean
+}
+
+export function FormBase({
+  children,
+  label,
+  description,
+  controlFirst,
+  horizontal,
+}: FormBaseProps) {
+  const field = useFieldContext()
+  const isInvalid = field.state.meta.isTouched && !field.state.meta.isValid
+  const labelElement = (
+    <>
+      <FieldLabel htmlFor={field.name}>{label}</FieldLabel>
+      {description && <FieldDescription>{description}</FieldDescription>}
+    </>
+  )
+  const errorElem = isInvalid && <FieldError errors={field.state.meta.errors} />
+
+  return (
+    <Field
+      data-invalid={isInvalid}
+      orientation={horizontal ? "horizontal" : undefined}
+    >
+      {controlFirst ? (
+        <>
+          {children}
+          <FieldContent>
+            {labelElement}
+            {errorElem}
+          </FieldContent>
+        </>
+      ) : (
+        <>
+          <FieldContent>{labelElement}</FieldContent>
+          {children}
+          {errorElem}
+        </>
+      )}
+    </Field>
+  )
+}
---
+++ packages/ui/src/components/form/form-hooks.tsx
@@ -0,0 +1,16 @@
+import { createFormHook, createFormHookContexts } from "@tanstack/react-form"
+import { FormInput } from "./form-input"
+
+const { fieldContext, formContext, useFieldContext, useFormContext } =
+  createFormHookContexts()
+
+const { useAppForm } = createFormHook({
+  fieldComponents: {
+    Input: FormInput,
+  },
+  formComponents: {},
+  fieldContext,
+  formContext,
+})
+
+export { useAppForm, useFieldContext, useFormContext }
