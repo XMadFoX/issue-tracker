aaddb310a25eb000ad9c7e7dd35d0bb8329c88a6
MadFox <madfox@laserflare.net>
2025-11-23 00:19:40 +0400

    feat(tss-web): add better-auth client

---
+++ apps/tss-web/src/lib/auth.ts
@@ -0,0 +1,8 @@
+import { createAuthClient } from "better-auth/react";
+import { env } from "src/env";
+export const authClient = createAuthClient({
+	/** The base URL of the server (optional if you're using the same domain) */
+	baseURL: env.VITE_API_URL,
+});
+
+export const { signIn, signUp, useSession } = authClient;
