053264feae1e3adb6588fa473e1785df167c64b2
MadFox <madfox@laserflare.net>
2025-11-25 02:05:54 +0400

    feat(tss-web): navigate from index to auth if not authed

--- apps/tss-web/src/routes/index.tsx
+++ apps/tss-web/src/routes/index.tsx
@@ -1,9 +1,20 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { useQuery } from "@tanstack/react-query";
+import { createFileRoute, Link, useNavigate } from "@tanstack/react-router";
+import { useEffect } from "react";
+import { useSession } from "src/lib/auth";
+import { orpc } from "src/orpc/client";
 
 export const Route = createFileRoute("/")({
 	component: RouteComponent,
 });
 
 function RouteComponent() {
-	return <div>Hello "/"!</div>;
+	const session = useSession();
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		if (!session.isPending && !session.data?.user) {
+			navigate({ to: "/auth" });
+		}
+	}, [session, navigate]);
 }
