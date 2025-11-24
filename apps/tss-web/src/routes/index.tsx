8d1f87933473c93958274103a71091be20a5bb86
MadFox <madfox@laserflare.net>
2025-11-25 02:06:15 +0400

    feat(tss-web/index): display simple workspace list

--- apps/tss-web/src/routes/index.tsx
+++ apps/tss-web/src/routes/index.tsx
@@ -12,9 +12,35 @@ function RouteComponent() {
 	const session = useSession();
 	const navigate = useNavigate();
 
+	const workspaces = useQuery(orpc.workspace.list.queryOptions({ input: {} }));
+
 	useEffect(() => {
 		if (!session.isPending && !session.data?.user) {
 			navigate({ to: "/auth" });
 		}
 	}, [session, navigate]);
+
+	return (
+		<div className="flex flex-col items-center justify-center max-w-md mx-auto">
+			{workspaces.data ? (
+				<div>
+					<h1>Your workspaces</h1>
+					<ul>
+						{workspaces.data.map((workspace) => (
+							<li key={workspace.id}>
+								<Link to={`/workspace/${workspace.slug}`}>
+									{workspace.name}
+								</Link>
+							</li>
+						))}
+					</ul>
+				</div>
+			) : (
+				<>
+					<p>You don't have any workspaces yet</p>
+					<Link to="/workspace/create">Create</Link>
+				</>
+			)}
+		</div>
+	);
 }
