750b2e1acd04432dc287d01bc13a726399a7ea8c
MadFox <madfox@laserflare.net>
2025-12-08 00:02:20 +0400

    feat(schema/issue-priorities): add indexing by workspace, unique rank per id

--- packages/db/src/features/tracker/issue-priorities.schema.ts
+++ packages/db/src/features/tracker/issue-priorities.schema.ts
@@ -1,22 +1,44 @@
 import { sql } from "drizzle-orm";
-import { integer, pgTable, text, timestamp } from "drizzle-orm/pg-core";
+import {
+	index,
+	integer,
+	pgTable,
+	text,
+	timestamp,
+	uniqueIndex,
+} from "drizzle-orm/pg-core";
 import { workspace } from "./tracker.schema";
 
-export const issuePriority = pgTable("issue_priority", {
-	id: text("id").primaryKey(),
-	workspaceId: text("workspace_id")
-		.notNull()
-		.references(() => workspace.id, { onDelete: "cascade" }),
-	name: text("name").notNull(),
-	// the lower the number, the higher the priority
-	rank: integer("rank").notNull(),
-	color: text("color"),
-	description: text("description"),
-	createdAt: timestamp("created_at", { withTimezone: true })
-		.defaultNow()
-		.notNull(),
-	updatedAt: timestamp("updated_at", { withTimezone: true })
-		.defaultNow()
-		.notNull()
-		.$onUpdate(() => sql`now()`),
-});
+export const issuePriority = pgTable(
+	"issue_priority",
+	{
+		id: text("id").primaryKey(),
+		workspaceId: text("workspace_id")
+			.notNull()
+			.references(() => workspace.id, { onDelete: "cascade" }),
+		name: text("name").notNull(),
+		// the lower the number, the higher the priority
+		rank: integer("rank").notNull(),
+		color: text("color"),
+		description: text("description"),
+		createdAt: timestamp("created_at", { withTimezone: true })
+			.defaultNow()
+			.notNull(),
+		updatedAt: timestamp("updated_at", { withTimezone: true })
+			.defaultNow()
+			.notNull()
+			.$onUpdate(() => sql`now()`),
+	},
+	(t) => {
+		return {
+			// list by workspace quickly
+			byWorkspace: index("issue_priority_workspace_idx").on(t.workspaceId),
+
+			// enforce single rank per workspace
+			uniqueWorkspaceRank: uniqueIndex("issue_priority_ws_rank_key").on(
+				t.workspaceId,
+				t.rank,
+			),
+		};
+	},
+);
