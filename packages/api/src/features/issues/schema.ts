ea947ab64be475207fdcc905ab4b6b76bae90694
MadFox <madfox@laserflare.net>
2025-12-17 01:39:19 +0400

    feat(api/issue): enforce create schema

--- packages/api/src/features/issues/schema.ts
+++ packages/api/src/features/issues/schema.ts
@@ -1,17 +1,24 @@
 import { issue } from "db/features/tracker/issues.schema";
 import { createInsertSchema } from "drizzle-zod";
 import { z } from "zod";
+import { teamInsertSchema } from "../teams/schema";
 import { workspaceInsertSchema } from "../workspaces/schema";
 
 export const issueInsertSchema = createInsertSchema(issue);
 
-export const issueCreateSchema = issueInsertSchema.omit({
-	id: true,
-	creatorId: true,
-	number: true,
-	createdAt: true,
-	updatedAt: true,
-});
+export const issueCreateSchema = issueInsertSchema
+	.omit({
+		id: true,
+		creatorId: true,
+		number: true,
+		createdAt: true,
+		updatedAt: true,
+	})
+	.extend({
+		workspaceId: workspaceInsertSchema.shape.id,
+		title: z.string().min(1).max(100),
+		teamId: teamInsertSchema.shape.id,
+	});
 
 export const issueListSchema = z.object({
 	workspaceId: workspaceInsertSchema.shape.id,
