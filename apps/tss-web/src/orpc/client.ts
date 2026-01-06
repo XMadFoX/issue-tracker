c36a0c1fc022143b6b0a4ec63dc6511d6e4a3f56
MadFox <madfox@laserflare.net>
2026-01-06 21:54:22 +0400

    feat(tss-web): replace orpc client creation with just client only instead of isomorphic

--- apps/tss-web/src/orpc/client.ts
+++ apps/tss-web/src/orpc/client.ts
@@ -1,36 +1,23 @@
 import { createORPCClient } from "@orpc/client";
 import { RPCLink } from "@orpc/client/fetch";
 import type { RouterClient } from "@orpc/server";
-import { createRouterClient } from "@orpc/server";
 import { createTanstackQueryUtils } from "@orpc/tanstack-query";
-import { router } from "@prism/api";
-import { createIsomorphicFn } from "@tanstack/react-start";
-import { getRequestHeaders } from "@tanstack/react-start/server";
+import type { router } from "@prism/api";
+import { createClientOnlyFn } from "@tanstack/react-start";
 import { env } from "src/env";
 
-const getORPCClient = createIsomorphicFn()
-	// for ssr
-	.server(() =>
-		createRouterClient(router, {
-			context: () => ({
-				headers: getRequestHeaders(),
-				// TODO: implement auth
-				auth: null,
-			}),
-		}),
-	)
-	.client((): RouterClient<typeof router> => {
-		const link = new RPCLink({
-			url: `${env.VITE_API_URL}/rpc`,
-			fetch: (request, init) => {
-				return globalThis.fetch(request, {
-					...init,
-					credentials: "include", // Include cookies for cross-origin requests
-				});
-			},
-		});
-		return createORPCClient(link);
+const getORPCClient = createClientOnlyFn((): RouterClient<typeof router> => {
+	const link = new RPCLink({
+		url: `${env.VITE_API_URL}/rpc`,
+		fetch: (request, init) => {
+			return globalThis.fetch(request, {
+				...init,
+				credentials: "include", // Include cookies for cross-origin requests
+			});
+		},
 	});
+	return createORPCClient(link);
+});
 
 export const client: RouterClient<typeof router> = getORPCClient();
 
