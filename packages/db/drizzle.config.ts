48b64803682987a0a5b63607dae3923da63a96bd
MadFox <madfox@laserflare.net>
2026-05-09 21:01:15 +0400

    fix(db): require statefull db url in drizzle conf

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -2,6 +2,9 @@ import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 import { env } from "./src/env";
 
+if (env.ENV_TYPE !== "server" || !env.DATABASE_URL)
+	throw new Error("Drizzle cli works only with statefull connection");
+
 export default defineConfig({
 	out: "./drizzle",
 	schema: "./src/schema.ts",
